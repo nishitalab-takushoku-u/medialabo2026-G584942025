a3516253536b6106ea5719c2f16bb34ffe74657a
mori kanta <258494@st.takushoku-u.ac.jp>
2026-06-18 16:11:43 +0900

    第3回編集済

--- practice-js/obj-practice.js
+++ practice-js/obj-practice.js
@@ -16,3 +16,12 @@ let gakka = [
 
 //////////////// ここから下にプログラムを書きたそう!
 
+console.log(campus.address);
+
+for (let r of campus.buildingD) {
+	console.log(r);
+}
+
+for (let g of gakka) {
+	console.log(g.name);
+}
